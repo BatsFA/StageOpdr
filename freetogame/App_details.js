78772663c6cebdd4397255f79bf1a0ed6640c4a9
Florian-Bats <81744660+Florian-Bats@users.noreply.github.com>
2024-01-15 14:41:49 +0100

    Added styling changes

--- freetogame/App_details.js
+++ freetogame/App_details.js
@@ -5,22 +5,46 @@ export default function AppDetails({ route }) {
   const { game } = route.params;
 
   return (
-    <View style={styles.gameContainer}>
-      <Text>App Details</Text>
-      <Text>Name: {game.title}</Text>
-      <Text>ID: {game.id}</Text>
-      <Text>------------------------</Text>
+    <View style={styles.container}>
+      <View style={styles.detaillist}>
+        <Text style={styles.titlelist}>App Details</Text>
+        <View style={styles.detailcontainer}>
+          <Text style={styles.gameTitle}>{game.title}</Text>
+          <Text style={styles.gameId}>ID: {game.id}</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    gameContainer: {
+    container: {
         flex: 1,
-        width: '45%',
+        width: '100%',
+        backgroundColor: '#fff',
+    },
+    detaillist: {
+        alignItems: 'center',
+        width: '100%',
+    },
+    titlelist: {
+        paddingBottom: 10,
+        textAlign: 'center',
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    detailcontainer: {
+        width: '85%',
         borderWidth: 1,
         borderColor: '#ccc',
         margin: 5,
         padding: 5,
-      },
+
+    },
+    gameTitle: {
+        textAlign: 'center',
+    },
+    gameId: {
+      
+    },
 });
